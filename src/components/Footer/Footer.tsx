6084e4d80a9010b6f1df2d7953353ecc35bfb073
Alex Santos <devalexsantos@gmail.com>
2023-06-18 18:37:58 -0300

    changing footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,8 +3,7 @@ import Link from "next/link";
 export default function Footer(){
     return(
         <footer className="w-full m-auto flex justify-center text-sm mt-10 mb-10 p-4 bg-gray-900">
-            <p className="text-center">©Copyright - Todos os direitos reservados ao Mano_Imp. 
-            Este site foi criado por <span><Link href="https://guia.gg" target="_blank" className="text-yellow-500 underline">GUIA.GG, crie um você também.</Link></span>.</p>
+            <p className="text-center">©Copyright - Todos os direitos reservados ao Mano_Imp. Este site não tem fins lucrativos e foi criado por um fã. Conheça o <Link href="https://d4guias.com" className="font-bold underline text-yellow-500" target="_blank">D4 Guias</Link>.</p>
         </footer>
     )
 }
